7c70ee429c9434a93d9741e3e9fe4935604e0b6c
VIKAS <singhvikas0491@gmail.com>
2023-04-23 09:11:08 +0530

    variable completed

---
+++ Variable/PractiseQuestionOne.js
@@ -0,0 +1,42 @@
+//create a variable of type string and try to //add a number to it
+let a="vikas"
+let b=7
+let c=a+b
+console.log(c)
+
+//use typeof operator to find the datattype //of variables in first question
+console.log(typeof(a+b))
+
+//create a const object
+const obj={
+name:"vikas",
+section:1,
+isPrincipal:false
+}
+console.log(obj)
+//obj=789  //this will give error
+
+
+//try to add a new key to the object you creat in the above //question
+const obj1={
+name:"First_name",
+section:1,
+isPrincipal:false
+}
+console.log(obj1)
+obj1['name2']="Second_name"
+console.log(obj1)
+
+//create a dictionary in javascript
+const dict={
+Broigus:"Angry or irritated",
+By_Catch:"A catch of fish that cannot be put to commercial use",
+Blert:"A cowardly person, someone who is weak",
+Comp:"Providing products or services free of charge as a token of appreciation, a favor"
+}
+
+console.log(dict)
+console.log(dict.Broigus)
+console.log(dict['Broigus'])
+
+
