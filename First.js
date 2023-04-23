5a40b33c4d8da13bb4f4ef2946f39a3dda225a2c
VIKAS <singhvikas0491@gmail.com>
2023-04-23 08:37:02 +0530

    Variable demonstration

---
+++ First.js
@@ -0,0 +1,8 @@
+console.log("the sum of 8,24,33")
+
+function addThreeNumbers(a,b,c){
+	return a+b+c;
+}
+
+let c=addThreeNumbers(8,24,33)
+console.log(c)
---
+++ Variable/variable.js
@@ -0,0 +1,43 @@
+//Variables in java
+var a= 67
+let b=45
+console.log(a)
+console.log(b)
+b="vikas"
+var a="vivek"
+console.log(a)
+//let b="singh"//thus it is dynamically type program and you //can't again declare b if its declared using let
+console.log(b)
+
+//object in javascript
+console.log("creating object")
+const obj1={
+name:"vikas",
+phoneNo:"9820682522",
+marks:"456"
+}
+console.log(obj1.marks)
+
+//create a var of type string and try to add number to it
+console.log("program 1")
+let s="stringVariable"
+console.log(s)
+s=456
+console.log("changed same var to number "+s)
+
+console.log("Exploring primitive data types ")
+//nn bb ss u
+let h=null;
+let i=345;
+let j=true;
+//let d=BigInt("4567")
+let e="Vikas"
+//let f=Symbol("My name is Vikas Singh")
+let g=undefined
+console.log(h+i+j+e+g)
+console.log(typeof i)
+
+
+
+
+
